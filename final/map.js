e4d3dc656ace3695759da6fcaeebb0031223eecb
aedepalma <depalma.ad@northeastern.edu>
2022-04-24 21:10:07 -0400

    buttons and styles

--- final/map.js
+++ final/map.js
@@ -33,79 +33,86 @@ d3.json("usa.json")
 
                 states.enter().append("path")
                     .attr("d", path)
-                    .attr("fill", "#008080")
+                    .attr("fill", "#666666")
                     .style("stroke", "#FFFFFF")
 
 
 
                 //circles
 
-                var dots = map.selectAll("circle")
-                    .data(csvData);
 
-                dots
-                    .transition()
-                    .duration(1000)
-                    .attr("cx", 0)
-                    .attr("cy", 0)
+                function drawCircles(year) {
 
-                var dotEnter = dots.enter().append("circle")
-                    .attr("class", "dots")
-                    .attr("r", function (d) {
-                        return d['2016-01'] * 0.00825
-                    })
-                    .attr("cx", function (d) {
-                        var coords = proj([d.long, d.lat])
-                        return coords[0];
-                    })
-                    .attr("cy", function (d) {
-                        var coords = proj([d.long, d.lat])
-                        return coords[1];
-                    })
-                    .transition()
-                    .duration(1000)
-                    .style("fill", function (d) {
-                        if (d['2016-01'] < 1200)
-                            return "#ACC2D8"
-                        else if (d['2016-01'] < 2300)
-                            return "white"
-                    });
+                    var dots = map.selectAll("circle")
+                        .data(csvData);
 
-                dots.merge(dotEnter)
-                    .on("mousemove", function (event, d) {
-                        console.log("tooltip data:", d);
-                        var tooltip = d3.select("#tooltip")
-                            .data(csvData)
-                            .style("display", "block")
-                            .style("top", event.pageY + 10 + "px")
-                            .style("left", event.pageX + 10 + "px")
-                            .html("City: " + d.city + "<br />Rent: $" + d['2016-01'])
-                    })
-                    .on("mouseout", function () {
-                        d3.select("#tooltip")
-                            .style("display", "none");
-                    });
+                    dots
+                        .transition()
+                        .duration(1000)
+                        .attr("cx", 0)
+                        .attr("cy", 0)
 
+                    var dotEnter = dots.enter().append("circle")
+                        .attr("class", "dots")
+                        .attr("r", function (d) {
+                            return d[year + '-01'] * 0.00825
+                        })
+                        .attr("cx", function (d) {
+                            var coords = proj([d.long, d.lat])
+                            return coords[0];
+                        })
+                        .attr("cy", function (d) {
+                            var coords = proj([d.long, d.lat])
+                            return coords[1];
+                        })
+                        .transition()
+                        .duration(1000)
+                        .style("fill", function (d) {
+                            if (d[year + '-01'] < 1500)
+                                return "#84CBCB"
+                            else if (d[year + '-01'] < 2300)
+                                return "#008080"
+                        });
 
+                    dots.merge(dotEnter)
+                        .on("mousemove", function (event, d) {
+                            console.log("tooltip data:", d);
+                            var tooltip = d3.select("#tooltip")
+                                .style("display", "block")
+                                .style("top", event.pageY + 10 + "px")
+                                .style("left", event.pageX + 10 + "px")
+                                .html("City: " + d.city + "<br />Rent: $" + d[year + '-01'])
+                        })
+                        .on("mouseout", function () {
+                            d3.select("#tooltip")
+                                .style("display", "none");
+                        });
+                }
+
+                d3.selectAll(".button").on("click", function () {
+                    var buttonYear = d3.select(this).html();
+                    console.log(buttonYear)
+                    drawCircles(buttonYear);
+
+                })
 
             })
+
+        //zoom
+        var zoom = d3.zoom()
+            .translateExtent([
+                [0, 0], [width, height]
+            ])
+            .scaleExtent([1, 8])
+            .on("zoom", zoomed);
+
+        function zoomed(event) {
+            map.attr("transform", event.transform);
+        }
+
+        svg.call(zoom);
+
+
+
+
     })
-
-//zoom
-var zoom = d3.zoom()
-    .translateExtent([
-        [0, 0], [width, height]
-    ])
-    .scaleExtent([1, 8])
-    .on("zoom", zoomed);
-
-function zoomed(event) {
-    map.attr("transform", event.transform);
-}
-
-svg.call(zoom);
-
-
-
-
-
